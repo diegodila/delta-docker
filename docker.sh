4bd5f9a499f7f0640fc221418b5292a73fe510c4
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2023-12-15 09:21:30 -0300

    adding new features

--- docker.sh
+++ docker.sh
@@ -1,3 +1,7 @@
+echo "Digite sua senha" ; read -s PASSWORD_JUPYTER
 docker volume create --name volume_delta_lake --opt type=none --opt device=$HOME/Projects/delta-docker/workdir --opt o=bind
 docker run --name delta_quickstart --mount source=volume_delta_lake,target=/opt/spark/work-dir \
--d --rm -p 8889-8890:8888-8889 delta_quickstart
+-e JUPYTER_TOKEN="easy" \
+-e PASSWORD="$PASSWORD_JUPYTER" \
+--restart unless-stopped \
+-d -p 8889-8890:8888-8889 delta_quickstart
